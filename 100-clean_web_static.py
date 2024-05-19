62f626ef7ecf4235becbdd7df8f60c874ba0d5a5
EngineerLucky <lucksehlatshwayo19@gmail.com>
2024-05-19 14:17:06 -0700

    100-clean_web_static.py

---
+++ 100-clean_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Deletes outdated archives
+"""
+
+import os
+from fabric.api import *
+
+env.hosts = ['54.154.134.103', '34.207.253.124']
+
+
+def do_clean(number=0):
+    """Deletesoutof date archives.
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
